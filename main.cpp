6fb96e975a546d7ab67b4e4e0f7e7b91e2953db2
Ivan Semyonov <gsgoldsmilegs@gmail.com>
2024-02-03 12:39:25 +0300

    initial commit

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <string>
+#include <vector>
 
 // Function to fill two-dimensional dynamic array in random numbers from 1 to 70
 void fillArray(int** arr, int rows, int cols) {
@@ -80,10 +81,94 @@ void task3(){
 
 
 }
+
+// Structure for representing a user account
+struct Account {
+    std::string username;
+    std::string password;
+    int id;
+};
+// Account storage vector
+std::vector<Account> accounts;
+// Function for adding a new account
+void addAccount() {
+    Account newAccount;
+    std::cout << "input username: ";
+    std::cin >> newAccount.username;
+    std::cout << "input password: ";
+    std::cin >> newAccount.password;
+    newAccount.id = accounts.size() + 1;
+    accounts.push_back(newAccount);
+    std::cout << "account added" << std::endl;
+}
+// Function to delete an existing account
+void deleteAccount() {
+    int id;
+    std::cout << "input account id to delete: ";
+    std::cin >> id;
+
+    for (auto it = accounts.begin(); it != accounts.end(); ++it) {
+        if (it->id == id) {
+            accounts.erase(it);
+            std::cout << "accoutn deleted!!!" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "account doesnt exist" << std::endl;
+}
+// Function to update account information
+void updateAccount() {
+    int id;
+    std::cout << "input account id to update: ";
+    std::cin >> id;
+
+    for (auto& acc : accounts) {
+        if (acc.id == id) {
+            std::cout << "input new username: ";
+            std::cin >> acc.username;
+            std::cout << "input new password: ";
+            std::cin >> acc.password;
+            std::cout << "account data updated" << std::endl;
+            return;
+        }
+    }
+
+    std::cout << "account doesnt exist" << std::endl;
+}
+// Function to display information about all accounts
+void printAllAccounts() {
+    if (accounts.empty()) {
+        std::cout << "list of accounts empty." << std::endl;
+        return;
+    }
+
+    std::cout << "list of all accounts:" << std::endl;
+    for (const auto& acc : accounts) {
+        std::cout << "ID: " << acc.id << ", username: " << acc.username << ", password: " << acc.password << std::endl;
+    }
+}
 // Function to solve task4
 void task4(){
-    std::cout<<"test";
-    std::cout << std::endl;
+    void (*funcPtrArr[])() = {addAccount, deleteAccount, updateAccount, printAllAccounts};
+    while (true) {
+        std::cout << std::endl << "Select action:"<< std::endl
+                  << "1. Add new account" << std::endl
+                  << "2. Delete account" << std::endl
+                  << "3. Update information about account" << std::endl
+                  << "4. Show information about all accounts" << std::endl
+                  << "5. Close program" << std::endl;
+        int choice;
+        std::cin >> choice;
+        if (choice < 1 || choice > 5) {
+            std::cout << "Incorrect input. Try again" << std::endl;
+            continue;
+        }
+        if (choice == 5) {
+            break;
+        }
+        funcPtrArr[choice - 1]();
+    }
 }
 
 
@@ -115,4 +200,3 @@ int main() {
     std::cout << "End program work." << std::endl;
     return 0;
 }
-
